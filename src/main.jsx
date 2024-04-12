93f74518a2df32572efe96fccf658a01e6b349ec
Cassandra Lee <cassandratlee@gmail.com>
2024-04-12 14:45:45 -0230

    Add React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
+import Test from "./test";
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "test",
+    element: <Test />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/test.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+    return (
+      <div>
+        <h1>Hello from the test page!</h1>
+      </div>
+    );
+  };
+  
+  export default Test;
