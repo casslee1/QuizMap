e737e5654c2afba723dda915b57ff5377a066d2f
Cassandra Lee <cassandratlee@gmail.com>
2024-04-28 17:44:36 -0230

    Switch button navigation from link to onClick

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types';
 import styles from './button.module.css';
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 function Button(props) {
 
-    return (
-      <Link to={props.whereTo}><button className={styles[props.buttonStyle]}>{props.name}</button></Link>
+const navigate = useNavigate()
+
+const handleClick=()=>{
+  navigate(props.whereTo);
+}
+
+return (
+      <button onClick={() => handleClick()} className={styles[props.buttonStyle]}>{props.name}</button>
     );
   }
 
@@ -17,29 +23,3 @@ Button.propTypes = {
   };
 
   export default Button;
-
-
-
-/*function Button(props) {
-      
-    const handleClick = () => {
-       
-     };
- 
- 
- 
-     return (
-       <Link to={props.whereTo}><button className={styles[props.buttonStyle]}>{props.name}</button></Link>
-     );
-   }
- 
-   Button.propTypes = {
-     name: PropTypes.string,
-     onClick: PropTypes.func,
-     buttonStyle: PropTypes.string,
-     whereTo: PropTypes.string  
-   };
- 
-   export default Button;
- 
- onClick={handleClick}*/
