20ab2dc3bb45b81dbb2be7619afa33416683e284
Cassandra Lee <cassandratlee@gmail.com>
2024-05-06 16:38:38 -0230

    Added background

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -21,15 +21,17 @@ const App = () => {
         <h1 className="tenor-sans-regular">QuizMap</h1>
         </div>
         
+        <div className="compassContainer">
+          <img src={CompassRose} alt="Compass Rose"/>
+        </div>
+
         <Description descText="Welcome! Answer a series of trivia questions by finding the location of the answer on a map." />
       
         <div>
         <Button name="Play" buttonStyle="playButton" handleClick={()=>{navigate("game")}}/>
         </div>
 
-        <div className="compassContainer">
-          <img src={CompassRose} alt="Compass Rose"/>
-        </div>
+      
       </div>
 
     </div>
