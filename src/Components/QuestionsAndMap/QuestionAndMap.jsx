087d481d1232a27f107b2c566c8fe36a74f84a87
Cassandra Lee <cassandratlee@gmail.com>
2024-05-11 12:07:19 -0230

    Add map with React Leaflet

--- src/Components/QuestionsAndMap/QuestionAndMap.jsx
+++ src/Components/QuestionsAndMap/QuestionAndMap.jsx
@@ -1,18 +1,35 @@
 import styles from './questionAndMap.module.css';
 import PropTypes from 'prop-types';
-
+import { useRef } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 
 
 function QuestionAndMap(props) {
-
-  
+    const mapRef = useRef(null);
+    const latitude = 51.505;
+    const longitude = -0.09;
       
     return (
     <div>
 
     <p>{props.questionDisplay}</p>
-    <div className={styles.map}> <p>The Map</p> </div>
+    
+    <div className={styles.map}>
+    <MapContainer
+      center={[latitude, longitude]}
+      zoom={0}
+      ref={mapRef}
+      style={{height: "500px", width: "400px"}}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* Additional map layers or components can be added here */}
+    </MapContainer>
+    </div>
      
     </div>
     );
@@ -24,3 +41,6 @@ QuestionAndMap.propTypes = {
 }
 
 export default QuestionAndMap;
+
+
+
--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Description from "../Components/Description/Description";
 import Button from "../Components/Button/Button";
 import CompassRose from "../SiteImages/CompassRose.png";
-import "../Styles/index.css"
+import "../Styles/index.css";
 
 
 const App = () => {
@@ -16,29 +16,45 @@ const App = () => {
   return (
     <div className="backgroundLanding">
 
-      <div className="appWrapper">
-        <div>
-        <h1 className="tenor-sans-regular">QuizMap</h1>
-        </div>
-        
-        <div className="compassContainer">
-          <img src={CompassRose} alt="Compass Rose"/>
-        </div>
+<div className="appWrapper">
+<div>
+<h1 className="tenor-sans-regular">QuizMap</h1>
+</div>
 
-        <Description descText="Welcome! Answer a series of trivia questions by finding the location of the answer on a map." />
-      
-        <div>
-        <Button name="Play" buttonStyle="playButton" handleClick={()=>{navigate("game")}}/>
-        </div>
+<div className="compassContainer">
+  <img src={CompassRose} alt="Compass Rose"/>
+</div>
 
-      
-      </div>
+<Description descText="Welcome! Answer a series of trivia questions by finding the location of the answer on a map." />
+
+<div>
+<Button name="Play" buttonStyle="playButton" handleClick={()=>{navigate("game")}}/>
+</div>
+
+</div>
 
     </div>
   );
 };
 
 export default App;
+
+/*<div className="appWrapper">
+<div>
+<h1 className="tenor-sans-regular">QuizMap</h1>
+</div>
+
+<div className="compassContainer">
+  <img src={CompassRose} alt="Compass Rose"/>
+</div>
+
+<Description descText="Welcome! Answer a series of trivia questions by finding the location of the answer on a map." />
+
+<div>
+<Button name="Play" buttonStyle="playButton" handleClick={()=>{navigate("game")}}/>
+</div>
+
+</div>*/
 
 
   /*<nav>
