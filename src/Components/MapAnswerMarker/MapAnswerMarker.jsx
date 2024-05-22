f976a340368c9220be4d20eaab4b2586896a5e29
Cassandra Lee <cassandratlee@gmail.com>
2024-05-22 19:42:40 -0230

    Marker component

--- src/Components/MapAnswerMarker/MapAnswerMarker.jsx
+++ src/Components/MapAnswerMarker/MapAnswerMarker.jsx
@@ -8,21 +8,49 @@ function MapAnswerMarker(props){
     const zoomLevel = useMapEvents({
       zoomend(){
         setZoom(zoomLevel.getZoom());
-        console.log("Zoom is" + zoomLevel.getZoom());
+        console.log("Zoom is " + zoomLevel.getZoom());
       },
     })
   
-    return zoom >= 4 ? ( <Marker position={[props.questionInfo.lat, props.questionInfo.lon]}>
+    return zoom >= 4 ? ( <Marker position={[props.currentLat, props.currentLon]}><Popup>{props.currentAnswer}</Popup></Marker>) : null;
+  
+  }
+
+  MapAnswerMarker.propTypes = {
+    currentLat: PropTypes.number,
+    currentLon: PropTypes.number, 
+    currentAnswer: PropTypes.string
+  }
+  
+  export default MapAnswerMarker;
+
+
+  /*import PropTypes from 'prop-types';
+import { useState} from "react";
+import { Marker, Popup, useMapEvents} from "react-leaflet";
+
+function MapAnswerMarker(props){
+    const [zoom, setZoom] = useState(null);
+  
+    const zoomLevel = useMapEvents({
+      zoomend(){
+        setZoom(zoomLevel.getZoom());
+        console.log("Zoom is " + zoomLevel.getZoom());
+      },
+    })
+     
+  
+  return zoom >= 4 ? ( <Marker position={[51.50825260251752, -0.07601367591891797]}>
       <Popup>
-        {props.questionInfo.answerText}
+        {props.currentAnswer}
       </Popup>
     </Marker>) : null;
-  
-  }
-  
-  MapAnswerMarker.propTypes = {
-    questionInfo: PropTypes.object
-  }
-  
 
-  export default MapAnswerMarker;
+      MapAnswerMarker.propTypes = {
+    currentLat: PropTypes.number,
+    currentLon: PropTypes.number, 
+    currentAnswer: PropTypes.string
+
+  }
+  
+  */
--- src/Components/QuestionAndMap/QuestionAndMap.jsx
+++ src/Components/QuestionAndMap/QuestionAndMap.jsx
@@ -10,8 +10,8 @@ function QuestionAndMap(props) {
  const mapRef = useRef(null);
   const latitude = 51.505;
   const longitude = -0.09;
-     
-
+  
+  
   return (
     <div>
 
@@ -29,9 +29,10 @@ function QuestionAndMap(props) {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-          <MapAnswerMarker />
+          <MapAnswerMarker currentLat={props.questionInfo.lat} currentLon={props.questionInfo.lon} currentAnswer={props.questionInfo.answerText} />
 
-         </MapContainer>
+        </MapContainer>
+
       </div>
      
     </div>
@@ -48,48 +49,6 @@ export default QuestionAndMap;
 
 
 
-/*function QuestionAndMap(props) {
- const mapRef = useRef(null);
-  const latitude = 51.505;
-  const longitude = -0.09;
-     
-
-  return (
-    <div>
-
-      <p>{props.questionInfo.questionText}</p>
-    
-      <div className={styles.map}>
-        <MapContainer
-          center={[latitude, longitude]}
-          zoom={0}
-          ref={mapRef}
-          style={{height: "500px", width: "400px"}}
-    >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-
-          <Marker position={[props.questionInfo.lat, props.questionInfo.lon]}>
-            <Popup>
-              {props.questionInfo.answerText}
-            </Popup>
-          </Marker>
-
-        </MapContainer>
-      </div>
-     
-    </div>
-  );
-    
-}
-
-QuestionAndMap.propTypes = {
-    questionInfo: PropTypes.object
-}
-
-export default QuestionAndMap;*/
 
 
 
